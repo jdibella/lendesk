0776664076d3a322833b74372b7779b2c39f68bc
Jake DiBella <jpdibella@gmail.com>
2018-04-06 17:23:39 -0700

    First pass at implementation (csv, html outputs)

---
+++ readme.rb
@@ -0,0 +1,15 @@
+# Photo Gps Info Extractor
+
+This program can be run via:
+* `ruby app.rb` (defaults to current directory)
+* `ruby app.rb /path/to/folder` (for `csv` output, saved to `output.csv`)
+* `ruby app.rb /path/to/folder html` (for `html` output, saved to `output.html`)
+
+## Dependencies
+
+### Libexif
+* `brew install libexif`
+* `gem install exif`
+
+### CSV
+* `gem install csv`
